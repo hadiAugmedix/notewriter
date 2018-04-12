195ea05bb27fce0767614667db0a63ac9f152b32
Habib Hadi <habibullah@augmedix.com>
2018-04-12 16:01:16 +0600

    init

---
+++ src/js/Example.js
@@ -0,0 +1,7 @@
+class Example {
+    init() {
+        console.log('From example classsss!');
+    }
+}
+
+export default Example;
---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+const extractPlugin = new ExtractTextPlugin({
+    filename: 'main.css'
+ });
+
+module.exports = {
+    entry: "./src/js/app.js",
+    output: {
+        path: __dirname + "/dist",
+        filename: "bundle.js"
+    },
+    module: {
+        rules: [
+            { test: /\.css$/, use: [ 'style-loader', 'css-loader' ] },
+            { test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" },
+            { test: /\.scss$/, use: extractPlugin.extract({ use: ["css-loader", "sass-loader"], fallback: "style-loader" }) }
+        ],
+    },
+    plugins: [
+        extractPlugin
+    ]
+}
